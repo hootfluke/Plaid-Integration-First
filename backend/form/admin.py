8327c904f3438ffd9be5b72d111e36ba7eeea6e4
Kristofer Hoetmer <hootfluke@gmail.com>
2024-07-20 15:02:07 -0400

    Addition of admin backend and django rest framework

--- backend/form/admin.py
+++ backend/form/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Form
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
 
 # Register your models here.
+
+admin.site.register(Form, FormAdmin)
